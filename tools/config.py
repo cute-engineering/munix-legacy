41373e2f28338f287d89ad422d4b4c2205ea3df6
d0p1 <contact@d0p1.eu>
2021-02-19 13:37:47 +0100

    build: Use custom python script instead of kconfig-frontend

---
+++ tools/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import kconfiglib
+import menuconfig
+import argparse
+
+kconfig = kconfiglib.Kconfig("Kconfig")
+
+
+def generate_header(dest):
+    kconfig.load_config('.config')
+    kconfig.write_autoconf(
+        dest, '#ifndef _COMMON_CONFIG_H\n#define _COMMON_CONFIG_H\n\n')
+    # a bit hacky but there is no other way and I really hate '#pragma once'
+    with open(dest, 'a') as f:
+        f.write('\n#endif /* !_COMMON_CONFIG_H */\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--genheader', action='store', default=None)
+    parser.add_argument('--menuconfig', action='store_true')
+    args = parser.parse_args()
+
+    if args.genheader is not None:
+        generate_header(args.genheader)
+
+    if args.menuconfig:
+        menuconfig.menuconfig(kconfig)
