8dc91db39dac0660132b9233165fa45c23cf3d8e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-02-19 20:31:29 +0100

    meta: Use Allman style  BreakBeforeBraces.

--- src/library/string.c
+++ src/library/string.c
@@ -4,7 +4,8 @@ size_t
 strlen(const char *s)
 {
 	size_t i = 0;
-	while (s[i] != '\0') {
+	while (s[i] != '\0')
+	{
 		i++;
 	}
 	return i;
@@ -14,7 +15,8 @@ int
 strcmp(const char *s1, const char *s2)
 {
 	size_t idx = 0;
-	while (s1[idx] == s2[idx] && s1[idx]) {
+	while (s1[idx] == s2[idx] && s1[idx])
+	{
 		idx++;
 	}
 	return (int)s1[idx] - (int)s2[idx];
