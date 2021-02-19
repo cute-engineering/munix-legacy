0ab9412534e8731fdf99eee545b12d3abaa67649
Supercip971 <supercyp971@gmail.com>
2021-02-19 10:45:44 -0400

    libc: added ctypes function

---
+++ src/library/ctypes.c
@@ -0,0 +1,67 @@
+#include "ctype.h"
+
+int
+isspace(int c)
+{
+	if (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == '\v') {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int
+isalnum(int c)
+{
+	if (c <= 'z' && c >= 'a') {
+		return 1;
+	}
+	if (c <= 'Z' && c >= 'A') {
+		return 1;
+	}
+	if (c <= '9' && c >= '0') {
+		return 1;
+	}
+	return 0;
+}
+
+int
+isdigit(int c)
+{
+	if (c <= '9' && c >= '0') {
+		return 1;
+	}
+	return 0;
+}
+
+int
+isalpha(int c)
+{
+	if (c <= 'z' && c >= 'a') {
+		return 1;
+	}
+	if (c <= 'Z' && c >= 'A') {
+		return 1;
+	}
+	return 0;
+}
+
+int
+to_lower(int c)
+{
+	if (c >= 'A' && c <= 'Z') {
+		c -= 'A';
+		c += 'a';
+	}
+	return c;
+}
+
+int
+to_upper(int c)
+{
+	if (c >= 'a' && c <= 'z') {
+		c -= 'a';
+		c += 'A';
+	}
+	return c;
+}
---
+++ src/library/ctypes.h
@@ -0,0 +1,11 @@
+#ifndef _MUNIX_CTYPES_MATH_H
+#define _MUNIX_CTYPES_MATH_H
+
+int isspace(int c);
+int isalnum(int c);
+int isdigit(int c);
+int isalpha(int c);
+
+int to_lower(int c);
+int to_upper(int c);
+#endif /* !_MUNIX_CTYPES_MATH_H */
