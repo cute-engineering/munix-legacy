81cc19fa0893923b82d82d501780a4b0637dc358
supercyp <supercyp971@gmail.com>
2021-02-20 13:59:09 -0400

    libc: Added strnlen, strncmp, memset, and memcpy.
    * libc: added ctypes function
    
    * libc: added strnlen
    
    * libc: added strncmp
    
    * libc: added memset
    
    * libc: added memcpy
    
    * libc: use Allman braces
    
    * libc: fix memset unused blank line

--- src/library/string.h
+++ src/library/string.h
@@ -4,7 +4,11 @@
 #include <stddef.h>
 
 size_t strlen(const char *s);
+size_t strnlen(const char *s, size_t maxlen);
 
 int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t maxlen);
 
+void * memset(void *s, int c, size_t n);
+void * memcpy(void *dest, const void *src, size_t n);
 #endif /* !_MUNIX_LIBRARY_STRING_H */
